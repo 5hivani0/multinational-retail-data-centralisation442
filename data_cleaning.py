23c77275a2cfd76f31eb350b70200845feb937c1
Shivani Bhakta <shivanibhakta2000@gmail.com>
2023-11-21 15:59:35 +0000

    edit clean product

--- data_cleaning.py
+++ data_cleaning.py
@@ -85,8 +85,6 @@ class DataCleaning():
     
     def clean_product_data(self):
         self.df = self.df.apply(self.convert_product_weights)
-        valid_availability = ['Removed', 'Still_available']
-        self.df = self.df[self.df['removed'].isin(valid_availability)]
         self.df['date_added'] = pd.to_datetime(self.df['date_added'], errors='coerce')
         self.df = self.df.dropna()
         self.df = self.df.reset_index(drop=True)
