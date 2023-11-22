b714bcb5a69c14cc1965bc84249587f4351a4ec5
Shivani Bhakta <shivanibhakta2000@gmail.com>
2023-11-22 12:03:22 +0000

    edit

--- data_cleaning.py
+++ data_cleaning.py
@@ -98,13 +98,6 @@ class DataCleaning():
         return self.df
 
     def clean_product_data(self):
-        self.df = self.df.convert_product_weights()
-        valid_category = ['toys-and-games', 'sports-and-leisure', 'pets', 'homeware', 'health-and-beauty', 'food-and-drink', 'diy']
-        self.df = self.df[self.df['category'].isin(valid_category)]
-        valid_availability = ['Removed', 'Still_available']
-        self.df = self.df[self.df['removed'].isin(valid_availability)]
-        self.df['date_added'] = pd.to_datetime(self.df['date_added'], errors='coerce')
-        self.df = self.df.dropna()
-        self.df = self.df.reset_index(drop=True)
+        self.df = self.convert_product_weights()
         return self.df
 
