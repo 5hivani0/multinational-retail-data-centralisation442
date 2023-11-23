fe5b259a9455878f9bb21503c9a52d3ad8a8be59
Shivani Bhakta <shivanibhakta2000@gmail.com>
2023-11-23 10:57:12 +0000

    edit

--- data_extraction.py
+++ data_extraction.py
@@ -53,10 +53,14 @@ class DataExtractor():
     
 
 api_key = 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'
-json_s3_address = "https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json"
 data_extractor = DataExtractor()
-date_time_df = data_extractor.json_extract_from_s3(json_s3_address)
 
-cleaning_datetime_data = DataCleaning(date_time_df)
-cleaned_datetime_data = cleaning_datetime_data.clean_datetime_data()
-print(cleaned_datetime_data)
+number_stores_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
+store_endpoint_template = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod'
+
+num_stores = data_extractor.list_number_of_stores(number_stores_endpoint)
+stores_data_df = data_extractor.retrieve_stores_data(store_endpoint_template, num_stores)
+
+cleaning_store_data = DataCleaning(stores_data_df)
+cleaned_store_data = cleaning_store_data.clean_store_data()
+print(cleaned_store_data)
