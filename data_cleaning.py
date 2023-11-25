e03af4b2c04d1cc4c44c5a5a6ab489feb11aabfd
Shivani Bhakta <shivanibhakta2000@gmail.com>
2023-11-25 09:00:41 +0000

    edit

--- data_cleaning.py
+++ data_cleaning.py
@@ -27,12 +27,10 @@ class DataCleaning():
         """
         self.df['date_of_birth'] = pd.to_datetime(self.df['date_of_birth'], errors='coerce')
         self.df['join_date'] = pd.to_datetime(self.df['join_date'], errors='coerce')
-
-        # Cleaning country codes and validating them
-        # Replace invalid country codes with a default value or NaN
         self.df['country_code'] = self.df['country_code'].replace('GBB', 'GB')
+        valid_country_codes = ['DE', 'US', 'GB']
+        self.df = self.df[self.df['country_code'].isin(valid_country_codes)]
         self.df = self.df.dropna()
-
         return self.df
     
     def clean_card_data(self):
